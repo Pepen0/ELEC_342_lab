1a753f8d4765858cec9f94667e100fcb84d48dbd
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2024-12-14 01:33:07 -0500

    Created main loop

--- Exo_for_final/Question_11_1_ii.py
+++ Exo_for_final/Question_11_1_ii.py
@@ -10,3 +10,13 @@ x_k = [1, 1, 1, 0.5, 0.5, 0.5, 0, 0, 0]
 # Compute DTFS coefficients for 0 <= n <= 9
 n_values = np.arange(0, 10)
 D_n = []
+
+for n in n_values:
+    # DTFS coefficient computation
+    Dn = (1 / K0) * sum(x_k[k] * np.exp(-1j * n * Omega_0 * k) for k in range(K0))
+    D_n.append(Dn)
+
+# Compute magnitude and phase of D_n
+magnitudes = np.abs(D_n)
+phases = np.angle(D_n)
+
